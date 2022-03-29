0332acfa6167a7c50095e95e52066d423be06adc
RavinduSathsara <rsathsara330@gmail.com>
2022-03-29 19:13:05 +0530

    set username and password for login

--- index.js
+++ index.js
@@ -1,68 +1,25 @@
-const Joi = require("joi");
 const express = require("express");
 const app = express();
+const md5 = require("md5");
 
 app.use(express.json());
 
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Horror" },
-  { id: 3, name: "Romance" },
-];
+const genres = [{ userName: "Action", passWord: "admin123" }];
 
-app.get("/api/genres", (req, res) => {
+app.get("/api/login", (req, res) => {
   res.send(genres);
 });
 
-app.post("/api/genres", (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+app.post("/api/login", (req, res) => {
+  //   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = {
-    id: genres.length + 1,
-    name: req.body.name,
+    userName: req.body.userName,
+    passWord: md5(req.body.passWord),
   };
   genres.push(genre);
   res.send(genre);
 });
 
-app.put("/api/genres/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  genre.name = req.body.name;
-  res.send(genre);
-});
-
-app.delete("/api/genres/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
-  res.send(genre);
-});
-
-app.get("/api/genres/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
-  res.send(genre);
-});
-
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-
-  return Joi.validate(genre, schema);
-}
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3500;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
